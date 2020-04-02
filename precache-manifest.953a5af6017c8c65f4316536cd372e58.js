6d06236e5e4fba69500ef7f896db435799e922d9
Nagesh <nagesh.k@tecnolynx.com>
2020-04-02 22:10:35 +0530

    Updates

---
+++ precache-manifest.953a5af6017c8c65f4316536cd372e58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3683c043122a24d6fd5220e1da70fdbd",
+    "url": "/website-one/index.html"
+  },
+  {
+    "revision": "711cf213db968a645cf8",
+    "url": "/website-one/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8151e22e91774cee4711",
+    "url": "/website-one/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "711cf213db968a645cf8",
+    "url": "/website-one/static/js/2.2e471250.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/website-one/static/js/2.2e471250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8151e22e91774cee4711",
+    "url": "/website-one/static/js/main.987fcb15.chunk.js"
+  },
+  {
+    "revision": "9986eddb68be98f4dbdd",
+    "url": "/website-one/static/js/runtime-main.71ee4083.js"
+  }
+]);
